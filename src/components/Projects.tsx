1c0a24465f60b6643f741fca612025dda70b8fcc
ishan-sharma23 <d.sharmaishan23@gmail.com>
2026-03-13 19:43:34 +0530

    projects with linkn updated

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -7,22 +7,29 @@ const PROJECTS = [
     title: 'Fake News Detection System using Machine Learning ',
     desc: 'An AI-powered web application that detects fake news in real-time using NLP and ensemble machine learning, providing instant verification with confidence scores. Built with MERN stack featuring responsive pages, user authentication, and ML model integration achieving high classification accuracy.',
     stack: ['Python', 'Scikit-learn', 'NLTK', 'TF-IDF', 'Random Forest', 'Voting Classifier', 'JWT', 'Tailwind CSS'],
-    github: '#',
+    github: 'https://github.com/ishan-sharma23/fakenews-project',
   },
   {
     num: '02',
     title: 'Air Quality Monitoring and Prediction System ',
     desc: 'Developed a satellite-based machine learning model to estimate PM₂.₅ and PM₁₀ using Sentinel-5P, meteorological, and land surface data. The system generates spatial PM predictions and AQI mapping for regions with limited ground monitoring.',
     stack: ['Google Earth Engine', 'Python', 'Random Forest', 'Linear Regression', 'Sentinel-5P', 'Sentinel-2', 'ERA5'],
-    github: '#',
+    github: 'https://github.com/ishan-sharma23/Air-quality-monitoring-system',
     
   },
   {
-    num: '03',
+  num: '03',
+  title: 'Bank Transactions Report & Fraud Detection Dashboard',
+  desc: 'Interactive 3-page Power BI dashboard analyzing bank transactions with fraud detection, geospatial mapping, and drillthrough insights using DAX measures and conditional formatting.',
+  stack: ['Power BI', 'DAX', 'Power Query', 'Bing Maps', 'PowerPoint'],
+  github: 'https://github.com/ishan-sharma23/bank-transactions-powerbi',
+ },
+  {
+    num: '04',
     title: 'Personal Blog Page ',
     desc: 'Developed a personal travel and adventure blog to share trekking experiences, travel guides, and information about spiritual destinations across India. The platform helps readers discover lesser-known places and plan trips through detailed blogs and insights. Built to strengthen content creation, digital presence, and community engagement.',
     stack: ['WordPress', 'SEO tools', 'Google Analytics', 'optimization plugins'],
-    github: '#',
+    github: '#https://ishanhiking.com/',
   },
 ]
 
